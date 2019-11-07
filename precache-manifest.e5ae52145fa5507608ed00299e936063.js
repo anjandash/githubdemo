db6bf215db1f23b00550717af3902175e3dd5f3c
Anjan Karmakar <anjandash@Anjans-MacBook-Pro.local>
2019-11-07 10:55:02 +0100

    Updates

---
+++ precache-manifest.e5ae52145fa5507608ed00299e936063.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd85ff1767c29e369ebd5c372ade45e",
+    "url": "/githubdemo/index.html"
+  },
+  {
+    "revision": "bd371ba316478c269c66",
+    "url": "/githubdemo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4f34d34d837d33732cb2",
+    "url": "/githubdemo/static/js/2.1f526b1d.chunk.js"
+  },
+  {
+    "revision": "bd371ba316478c269c66",
+    "url": "/githubdemo/static/js/main.7efd5170.chunk.js"
+  },
+  {
+    "revision": "0a87510462b91d38b8b6",
+    "url": "/githubdemo/static/js/runtime-main.f0d6e7d6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/githubdemo/static/media/logo.25bf045c.svg"
+  }
+]);
